c54761298d69fcb6b80aaee67e845bd157146654
davidAg9 <shane_alpha9@outlook.com>
2022-03-07 15:41:13 +0000

    add third party mail

--- thetagateway/services/mail_service.go
+++ thetagateway/services/mail_service.go
@@ -1,5 +1,30 @@
 package services
 
+import (
+	"os"
+
+	"github.com/keep94/toolbox/mailer"
+)
+
 func SendTransactionalEmail() (bool, error) {
 	return true, nil
+}
+
+//send email to a sign user
+func SendSignUpEmail(userMail string) {
+	var email mailer.Email
+	pass := os.Getenv("MAILPASS")
+	myMailer := mailer.New("shane.qoubby@gmail.com", pass)
+	email.To = []string{userMail}
+	email.Subject = "Theta Email Verification Service"
+	//TODO:CREATE EMAIL TEMPLATE
+	emailTempLate := `
+	<h1>Click the link below to verify your email</h1>
+	<br>
+	<p>http://localhost:8080/customer/email/[token]</p>
+	`
+	email.Body = emailTempLate
+
+	myMailer.Send(email)
+
 }
